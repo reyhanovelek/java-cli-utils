cbb9cf02ef77e109b612c8f1ecb6a04f9f0e797d
reyhan ovelek <reyhanovelek@gmail.com>
2025-04-11 09:53:34 +0200

    Add greetTimeOfDay method

--- src/utils/HelloUtils.java
+++ src/utils/HelloUtils.java
@@ -4,4 +4,8 @@ public class HelloUtils {
     public static String sayHello(String name ){
         return "Hello "+name+"!";
     }
+    public static String greetTimeOfDay(String name ,String time){
+        return "Good "+ time +", " +time+ "!";
+
+    }
 }
